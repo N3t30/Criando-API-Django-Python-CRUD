6096e2901f5c446f938e1346e281c76aca9e8fcd
Neto Peixoto <joseneto55peixoto@gmail.com>
2024-10-19 19:30:32 -0300

    Criando modelos

--- api_rest/models.py
+++ api_rest/models.py
@@ -1,3 +1,14 @@
+
 from django.db import models
 
-# Create your models here.
+
+# Aplicação de cadastro de usuarios
+class user(models.Model):
+    user_nickname = models.CharField(
+        max_length=100, primary_key=True, default="")
+    user_name = models.CharField(max_length=160, default="")
+    user_email = models.EmailField(default=0)
+    user_age = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'Nickname: {self.user_nickname} | E-mail: {self.user_email}'
