aacf391980be87b73d7ea35fea1b4350d22b5603
Neto Peixoto <joseneto55peixoto@gmail.com>
2024-10-19 20:30:21 -0300

    GET(1/3) acessando todos os dados

--- api_rest/views.py
+++ api_rest/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.shortcuts import HttpResponse, JsonResponse, render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import user
+from .serializers import UserSerializer
+
+# #  Resumo de banco de dados, alguns comandos mais usados
+# def databaseEmdjango():
+
+#     data = user.objects.get(pk='user_nickname') #OBJETO
+#     # Busca um objeto único no banco de dados
+#     data = user.objects.filter(user_age='25')  # QUERYSET
+#     # Retorna uma lista de objetos que você tem critério de filtro
+#     data = user.objects.exclude(USER_AGE='25')  # QUERYSET
+#     # Retorna uma lista de objetos, excluindo aqueles
+#     data.save()
+#     #Salva o objeto atualno banco de dados
+#     data.delete()
+#     # Apagar o objeto atual do banco de dados
